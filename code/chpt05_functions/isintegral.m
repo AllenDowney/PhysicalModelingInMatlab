3327bd3b7b92c585a500418dc993c7b4c4175e60
wcs <wcs@sfu.ca>
2019-01-07 02:53:46 -0500

    A few scattered improvements.

--- code/chpt05_functions/isintegral.m
+++ code/chpt05_functions/isintegral.m
@@ -1,7 +1,7 @@
 function res = isintegral(x)
     if round(x) == x
-        res = 1;
+        res = 1;       % or... res = true
     else
-        res = 0;
+        res = 0;       % or... res = false
     end
 end
