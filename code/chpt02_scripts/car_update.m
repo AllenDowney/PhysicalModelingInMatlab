260df123a8ed774b9b93cbf0327e953df4e3c9c1
Allen Downey <downey@allendowney.com>
2019-01-07 16:04:43 -0500

    Updating Chapter 2 code

--- code/chpt02_scripts/car_update.m
+++
@@ -1,3 +0,0 @@
-atob = round(0.05*a) - round(0.03*b);
-a = a - atob
-b = b + atob
--- code/chpt02_scripts/fibonacci1.m
+++ code/chpt02_scripts/fibonacci1.m
@@ -1,7 +1,6 @@
-% computes a numerical approximation of the nth Fibonacci
-% number.  Precondition: you must assign a value to n before
-% running this script.  Postcondition: the result is stored
-% in ans.
+% Computes a numerical approximation of the nth Fibonacci number.  
+% Precondition: you must assign a value to n before running this script.
+% Postcondition: the result is stored in ans.
 
 s5 = sqrt(5);
 t1 = (1 + s5) / 2;
