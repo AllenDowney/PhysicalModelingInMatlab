cd96511ba04c931cd9bcc5d5cf18063076d1f4b6
Allen Downey <downey@allendowney.com>
2019-01-09 15:53:07 -0500

    Updating chapter 8 code

--- code/chpt08_odes/coffee.m
+++ code/chpt08_odes/coffee.m
@@ -1,37 +1,21 @@
+% Use ode45 to predict coffee temperature.
+
 function res = coffee()
-    %t = fzero(@temp_func1, 480)
-    %t = fzero(@temp_func2, 480)
-
-    temp_func1(440)
-    temp_func2(440)
+    t = 0;
+    y0 = 90;
+    r = rate_func(t, y0);
+    
+    [T, Y] = ode45(@rate_func, [0, 60], y0);
+    plot(T, Y)
+    xlabel('Time (minutes)')
+    ylabel('Temperature (C)')
+    saveas(gcf, '../../book/figs/coffee.eps', 'epsc')
+    
+    Y(end)
 end
 
-function res = temp_func1(t)
-    % mix first, then run for t seconds and report the final temp.
-    y_final = mix_func(90, 8, 20, 1)
-    diff = 90 - y_final
-    [T, Y] = ode45(@slope_func, [0,t], y_final);
-    diff = Y(end) - y_final
-    res = Y(end) - 60;
-end
-
-function res = temp_func2(t)
-    % run for t seconds, then mix and report final temp.
-    [T, Y] = ode45(@slope_func, [0,t], 90);
-    diff = Y(end) - 90
-    y = Y(end)
-    y_final = mix_func(y, 8, 20, 1);
-    diff = y_final - y
-    res = y_final - 60;
-end
-
-function res = slope_func(t,y)
-    r = 0.001;
+function res = rate_func(t, y)
+    k = 0.01;
     e = 20;
-    res = -r * (y - e);
+    res = -k * (y - e);
 end
-
-function res = mix_func(y1, v1, y2, v2)
-    res = (v1*y1 + v2*y2) / (v1+v2);
-end
-
--- code/chpt08_odes/rats.m
+++ code/chpt08_odes/rats.m
@@ -1,5 +1,21 @@
-function res = rats(t, y)
-    a = 0.02;
-    omega = 2 * 2*pi / 365;
-    res = a * y * (1 - cos(omega * t)) / 2;
+% Use ode45 to predict the rat population.
+
+function res = rats()
+    t = 31;
+    y = 2000;
+    res = rate_func(t, y);
+    
+    [T, Y] = ode45(@rate_func, [0, 365], 2000);
+    plot(T, Y)
+    xlabel('Time (days)')
+    ylabel('Population (rats)')
+    saveas(gcf, '../../book/figs/rats.eps', 'epsc')
+    
+    Y(end)
+end
+
+function res = rate_func(t, y)
+    a = 0.002;
+    omega = 2*pi / 365;
+    res = a * y * (1 - cos(omega * t));
 end
