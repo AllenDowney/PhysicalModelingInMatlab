ca690687801cf734a5154f3ca4b794d2bf2c05e9
Allen Downey <downey@allendowney.com>
2019-01-15 12:21:31 -0500

    Updating Chapter 13

---
+++ code/chpt12_optimization/optimization.m
@@ -0,0 +1,12 @@
+function optimization()
+    x = fzero(@error_func, 1)
+    
+    x = fminsearch(@error_func, 1)
+
+    [x, fval] = fminsearch(@error_func, 1)
+
+end
+
+function res = error_func(x)
+    res = x^2 - 2;
+end
