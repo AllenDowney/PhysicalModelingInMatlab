b7219931584d8a056590447235133b8dfaf816a8
Allen Downey <downey@allendowney.com>
2019-01-15 12:22:54 -0500

    Updating chap 12 and 13 code

---
+++ code/chpt12_optimization/baseball_anim.m
@@ -0,0 +1,68 @@
+function baseball3()
+    P = [0; 1];       % initial position in m
+    V = [40; 30];     % initial velocity in m/s
+    W = [P; V];       % initial condition
+    rate_func(0, W)
+    
+    tspan = 0:0.1:10;
+    options = odeset('Events', @event_func);
+    [T, M] = ode45(@rate_func, tspan, W, options);
+    
+    animate(T, M)
+end
+
+function animate(T,M)
+    X = M(:,1);
+    Y = M(:,2);
+
+    minmax = [min([X]), max([X]), min([Y]), max([Y])];
+
+    for i=1:length(T)
+        clf; hold on
+        axis(minmax);
+        plot(X(i), Y(i), 'o')
+        drawnow;
+        
+        if i < length(T)
+            dt = T(i+1) - T(i);
+            pause(dt);
+        end
+    end
+end
+
+function res = rate_func(t, W)
+    P = W(1:2);
+    V = W(3:4);
+
+    dPdt = V;
+    dVdt = acceleration(t, P, V);
+
+    res = [dPdt; dVdt];
+end
+
+function res = acceleration(t, P, V)
+    g = 9.8;                       % acceleration of gravity in m/s^2
+    a_gravity = [0; -g];
+
+    m = 0.145;                     % mass in kilograms
+    a_drag = drag_force(V) / m;
+    res = a_gravity + a_drag;
+end
+
+function res = drag_force(V)
+    C_d = 0.3;      % dimensionless
+    rho = 1.3;      % kg / m^3
+    A = 0.0042;     % m^2
+    v = norm(V);    % m/s
+
+    res = - 1/2 * C_d * rho * A * v * V;
+end
+
+function [value, isterminal, direction] = event_func(t, W)
+    value = W(2);
+    isterminal = 1;
+    direction = -1;
+end
+
+
+
