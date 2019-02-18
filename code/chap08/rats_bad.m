6d73b7d66cab656d97f8005ce099d5f6ec626624
Allen Downey <downey@allendowney.com>
2019-02-18 11:46:51 -0500

    Removing unneeded files

--- code/chap08/rats_bad.m
+++
@@ -1,5 +0,0 @@
-function res = rats_bad(t, y)
-    a = 0.02;
-    omega = 2 * 2*pi / 365;
-    r = a * y * (1 - cos(omega * t)) / 2;    % WRONG
-end
--- code/chap08/rats_plot.m
+++
@@ -1,5 +0,0 @@
-function res = rats_plot(t, y)
-    % plot(t, y, 'r.')
-    plot(t, y, 'r.', 'MarkerSize', 10)
-    res = rats(t, y);
-end
