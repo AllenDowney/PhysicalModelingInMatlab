be01434569709e0ee95b3dd7dcd0d308d9bccfd8
Allen Downey <downey@allendowney.com>
2020-04-29 19:11:06 -0400

    Updating links

--- links/set_urls.py
+++ links/set_urls.py
@@ -1,23 +1,31 @@
 import csv
 import re
+from glob import glob
+
 
 link_map = {}
 with open('urls.csv') as csvfile:
     reader = csv.reader(csvfile)
     for i, row in enumerate(reader):
         slashtag, destination = row
-        link_map[destination] = 'http://modsimpy.com/' + slashtag
+        link_map[destination] = 'https://greenteapress.com/matlab/' + slashtag
+
+for filename in glob('../*.tex'):
+    outname = filename.split('/')[-1]
+    fout = open(outname, 'w')
+
+    for line in open(filename):
 
 
-for line in open('../book.tex'):
-    matches = re.findall(r'\\url\{([^\}]*)', line)
-    if matches:
-        for url in matches:
-            key = url.replace('\\#', '#')
-            try:
-                short = link_map[key]
-                line = line.replace(url, short)
-            except KeyError:
-                pass
+        matches = re.findall(r'\\url\{([^\}]*)', line)
+        if matches:
+            for url in matches:
+                key = url.replace('\\#', '#')
+                try:
+                    short = link_map[key]
+                    line = line.replace(url, short)
+                except KeyError:
+                    pass
                 #print url
-    print(line, end='')
+        fout.write(line)
+    fout.close()
