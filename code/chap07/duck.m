b59685961b87d019d1aecc55d0884176da95295e
Allen Downey <downey@allendowney.com>
2021-05-26 10:31:08 -0400

    Moving code to the right chapters

--- code/chap07/duck.m
+++ code/chap07/duck.m
@@ -1,9 +1,13 @@
 function res = duck()
-    error = error_func(10)
+    error = error_func(2)
+    ezplot(@error_func, [0, 20])
+    res = fzero(@error_func, 2);
 end
 
-function res = error_func(h)
+function res = error_func(d)
     rho = 0.3;      % density in g / cm^3
     r = 10;         % radius in cm
-    res = h;
+    mass_duck = 4/3 * pi * r.^2 * rho;;
+    mass_water = pi / 3  * (3*r*d.^2 - d.^3);
+    res = mass_water - mass_duck;
 end
